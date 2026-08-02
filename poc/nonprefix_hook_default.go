6e5cab2eae15948c4e13b29f5339de7a8286222c
dorothhintz <81392970+dorothhintz@users.noreply.github.com>
2026-08-02 20:39:02 +0100

    test: add default no-op non-prefix hook

---
+++ poc/nonprefix_hook_default.go
@@ -0,0 +1,7 @@
+//go:build !nonprefixpoc
+
+package keccak
+
+import "github.com/consensys/linea-monorepo/prover/protocol/wizard"
+
+func applyNonPrefixPoCHook(_ *wizard.ProverRuntime, _ *KeccakSingleProvider, _ *[][]byte) {}
