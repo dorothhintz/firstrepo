d07137a8134260a6be6ebc83589c4077c94a828a
dorothhintz <81392970+dorothhintz@users.noreply.github.com>
2026-08-02 21:21:51 +0100

    test: keep honest packing in normal builds

--- poc/nonprefix_hook_default.go
+++ poc/nonprefix_hook_default.go
@@ -4,4 +4,6 @@ package keccak
 
 import "github.com/consensys/linea-monorepo/prover/protocol/wizard"
 
-func applyNonPrefixPoCHook(_ *wizard.ProverRuntime, _ *KeccakSingleProvider, _ *[][]byte) {}
+func applyNonPrefixPoCHook(_ *wizard.ProverRuntime, _ *KeccakSingleProvider, _ *[][]byte) bool {
+	return false
+}
